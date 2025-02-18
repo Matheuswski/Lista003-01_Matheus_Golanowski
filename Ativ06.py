77a6e6938b57f61a75a425a614d081434c09a587
MatheusG <mariaoglanowski@gmail.com>
2025-02-18 19:55:27 -0300

    atualização

--- Ativ06.py
+++ Ativ06.py
@@ -1,1 +1,8 @@
-cadidato = input
+nome_cidade = input("Qual a cidade ")
+votos_canndidato = int(input("quantidade de votos do candidato: "))
+qtde_votos = int(input("Qual a quantidade de votos: "))
+porcetagem = votos_canndidato/qtde_votos
+if porcetagem <= 0.50 :
+    print("Avera segundo turno na cidade: ",nome_cidade)
+else:
+    print("Não avera segundo turno ")
