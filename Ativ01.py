dd3c1b7ab26dab5451f8a02fdd51b39e2a073278
MatheusG <mariaoglanowski@gmail.com>
2025-02-17 22:10:25 -0300

    xi

--- Ativ01.py
+++ Ativ01.py
@@ -1,3 +1,12 @@
-investimentos = input("Voce deseja investir?: ")
-
-if valor = 
+quantidade = input("insira o grau de aceitação do investimento que deseja receber ")
+investimento = float (input("Agora, insira o valor do ivestimento "))
+if quantidade == "Bx":
+    if investimento < 1000.00:
+        print("invist na poupaça")
+    else:
+        print("Invest na renda fixa")
+if quantidade == "Al":
+    if investimento < 1000.00:
+        print("invist nos bitcoins")
+    else:
+        print("Invest na ações")        
